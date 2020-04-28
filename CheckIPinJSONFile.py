16f82cc8b125cfc99cf550f5c6729ec605321f38
Hal.Horton <hal.horton@loves.com>
2020-04-27 19:09:02 -0500

    removing superflous loop

--- CheckIPinJSONFile.py
+++ CheckIPinJSONFile.py
@@ -7,18 +7,15 @@ data = json.loads(deviceJSON)
 
 
 for record in data['interfaces']['interface']:
-    for name in record:
-        phyInterface = name
-    for lvl2Record in record.values():
-        for lvl2Record in lvl2Record.values():
-            ipAddress = lvl2Record
-            try:
-                if ipaddress.ip_address(ipAddress).is_private == True:
-                    print(f'{phyInterface} has an ip address of {ipAddress} which is a Private Address')
-                else:
-                    print(f'{phyInterface} has an ip address of {ipAddress} which is not a Private Address')
-            except ValueError:
-                print(f"{phyInterface} does not appear to have a valid ip address defined")
+    for interface in record:
+        ipAddress = record[interface]['ipv4']
+        try:
+            if ipaddress.ip_address(ipAddress).is_private == True:
+                print(f'{interface} has an ip address of {ipAddress} which is a Private Address')
+            else:
+                print(f'{interface} has an ip address of {ipAddress} which is not a Private Address')
+        except ValueError:
+            print(f"{interface} does not appear to have a valid ip address defined")
 
 
 
